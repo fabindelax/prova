485f548c4c0bba4de420544a16e2d75fc25d1ea4
fabindelax <113560771+fabindelax@users.noreply.github.com>
2022-10-19 21:55:04 -0300

    Add files via upload

---
+++ Prova 1.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+  int main (){
+  char op;
+  
+ printf ("escolha qual opcao deseja: ");
+ scanf("%c", &op);
+
+switch(letra){
+	
+case 'a': printf("
+	
+   printf("Digite dois valores: ");
+  scanf("%i,%i", &a, &b);	
+  
+  x = pow(a, 2) + pow(b, 2);
+  hip =sqrt(x);
+  seno = a / hip;
+  printf(" A hipotenusa vale %.3i e o seno vale %.3i", hip, seno);
+  "); break;
+case 'b': printf(" "); break;
+case 'c': printf(" "); break;
+default: printf("opcao invalida");
+}
+
+}
